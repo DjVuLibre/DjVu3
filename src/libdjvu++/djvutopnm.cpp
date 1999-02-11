b66693a9ae909fcd014362bb64fc548fd7e9b359
leonb <leonb>
1999-02-11 22:37:05 +0000

    Documented djvutopnm.cpp.

--- src/libdjvu++/djvutopnm.cpp
+++ src/libdjvu++/djvutopnm.cpp
@@ -7,10 +7,87 @@
 //C-  The copyright notice above does not evidence any
 //C-  actual or intended publication of such source code.
 //C-
-//C-  $Id: djvutopnm.cpp,v 1.9 1999-02-03 22:55:30 leonb Exp $
+//C-  $Id: djvutopnm.cpp,v 1.10 1999-02-11 22:37:05 leonb Exp $
 
-// File "$Id: djvutopnm.cpp,v 1.9 1999-02-03 22:55:30 leonb Exp $"
-// Author: Yann Le Cun 08/1997
+
+/** @name djvutopnm
+
+    {\bf Synopsis}
+    \begin{verbatim}
+        djvutopnm [options] djvufilename [pnmfilename]
+    \end{verbatim}
+
+    {\bf Description} --- File #"djvutopnm.cpp" illustrates how to decode and
+    render a DjVu file using class #DjVuImage#.  This program decodes all
+    variants of DjVu files, including the wavelet files produced by \Ref{c44},
+    and produces PNM files (see \Ref{PNM and RLE file formats}).
+
+    {\bf Arguments} --- Argument #djvufilename# is the name of the input DjVu
+    file.  A single dash #"-"# represents the standard input.  Argument
+    #pnmfilename# is the name of the output PNM file.  Omitting this argument
+    or specifying a single dash #"-"# represents the standard output.
+
+    {\bf Output Resolution} --- Three options control the resolution of the 
+    output PNM image.  At most one of these three options can be specified.
+    The default resolution, used when no other option is specified, is equivalent
+    to specifying #-scale 100#.
+    \begin{description}
+    \item[-N] This option (e.g #"-3"# or #"-19"#) specifies a subsampling
+       factor #N#.  Rendering the full DjVu image would create an image whose
+       dimensions are #N# times smaller than the DjVu image size.
+    \item[-scale N] This option takes advantage of the #dpi# field stored in
+       the #"INFO"# chunk of the DjVu image (cf. \Ref{DjVuInfo}).  Argument
+       #N# is a magnification percentage relative to the adequate resolution
+       for a 100dpi device such as a screen.
+    \item[-size WxH] This option provides total control on the resolution and
+       the aspect ratio of the image.  The vertical and horizontal resolutions
+       will be separately adjusted in such a way that the complete DjVu image
+       is rendered into a PNM file of width #W# and height #H#.
+    \end{description}
+    
+    {\bf Rendering Mode Selection} --- The default rendering mode merges all
+    the layers of the DjVu image and outputs an adequate PNM file. IW44 files
+    and Color DjVu files are always rendered as PPM files. Black and White
+    DjVu files are rendered as PBM files if the subsampling factor is 1.
+    Otherwise, they are rendered as PGM files because the resolution change
+    gives better results with anti-aliasing.  Three options alter this
+    default behavior.
+    \begin{description}
+    \item[-black] Renders only the foreground layer mask.  This mode does not
+       work with IW44 files because these files have no foreground layer mask.
+       The output file will be a PBM file if the subsampling factor is 1.
+       Otherwise the output file will be an anti-aliased PGM file.
+    \item[-foreground] Renders only the foreground layer on a white
+       background.  This mode works only with Color DjVu files. The output
+       file always is a PPM file.
+    \item[-background] Renders only the background layer. This mode works only
+       with Color DjVu files and IW44 files. The output file always is a PPM
+       file.
+    \end{description}
+
+    {\bf Other Options} --- The following two options are less commonly used:
+    \begin{description}
+    \item[-segment WxH+X+Y] Selects an image segment to render. Conceptually,
+       #djvutopnm# renders the full page using the specified resolution, and
+       then extracts a subimage of width #W# and height #H#, starting at
+       position (#X#,#Y#) relative to the bottom left corner of the page.
+       Both operations of course happen simultaneously.  Rendering a small
+       subimage is much faster than rendering the complete image.  Note that
+       the output PNM file will always have size #WxH#.
+    \item[-v] Causes #djvutopnm# to print abundant information about the
+       structure of the DjVu file, the compression ratios, the memory usage,
+       and the decoding and rendering times.
+    \end{description}
+
+    @memo
+    Decodes and renders a DjVu file.
+    @author
+    Yann Le Cun <yann@research.att.com>\\
+    Leon Bottou <leonb@research.att.com>
+    @version
+    #$Id: djvutopnm.cpp,v 1.10 1999-02-11 22:37:05 leonb Exp $# */
+//@{
+//@}
 
 #include <stdio.h>
 #include "GException.h"
