f5f6b37999f92b5e00c53ef7d285f15dcc10b0fa
leonb <leonb>
1999-03-17 19:24:56 +0000

    Changed copyright notice one more time

--- src/libdjvu++/IFFByteStream.cpp
+++ src/libdjvu++/IFFByteStream.cpp
@@ -1,30 +1,17 @@
 //C-  -*- C++ -*-
 //C-
-//C- This software contains software code or other information proprietary to
-//C- AT&T Corp. ("AT&T").  Unless you accept a license to use this software,
-//C- you shall not use, execute, compile, modify, redistribute, reverse
-//C- compile, disassemble, or otherwise reverse engineer the AT&T software or
-//C- any derived work of the AT&T software.  The text of a license can be
-//C- found in file "ATTLICENSE" or at the Internet website having the URL
-//C- "http://www.djvu.att.com/open".
+//C- Copyright (c) 1999 AT&T Corp.  All rights reserved.
 //C-
-//C- This software is provided to you "AS IS".  YOU "AS IS".  YOU ASSUME TOTAL
-//C- RESPONSIBILITY AND RISK FOR YOUR USE OF THEM INCLUDING THE RISK OF ANY
-//C- DEFECTS OR INACCURACIES THEREIN.  AT&T DOES NOT MAKE, AND EXPRESSLY
-//C- DISCLAIMS, ANY EXPRESS OR IMPLIED WARRANTIES OF ANY KIND WHATSOEVER,
-//C- INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-//C- OR FITNESS FOR A PARTICULAR PURPOSE, WARRANTIES OF TITLE OR
-//C- NON-INFRINGEMENT OF ANY INTELLECTUAL PROPERTY RIGHTS OR TRADEMARK RIGHTS,
-//C- ANY WARRANTIES ARISING BY USAGE OF TRADE, COURSE OF DEALING OR COURSE OF
-//C- PERFORMANCE, OR ANY WARRANTY THAT THE AT&T SOURCE CODE RELEASE OR AT&T
-//C- CAPSULE ARE "ERROR FREE" WILL MEET YOUR REQUIREMENTS.
+//C- This software may only be used by you under license from AT&T
+//C- Corp. ("AT&T"). A copy of AT&T's Source Code Agreement is available at
+//C- AT&T's Internet website having the URL <http://www.djvu.att.com/open>.
+//C- If you received this software without first entering into a license with
+//C- AT&T, you have an infringing copy of this software and cannot use it
+//C- without violating AT&T's intellectual property rights.
 //C-
-//C-     (C) AT&T Corp. All rights reserved.
-//C-     AT&T is a registered trademark of AT&T Corp.
-//C-
-//C- $Id: IFFByteStream.cpp,v 1.8 1999-03-16 20:21:29 leonb Exp $
+//C- $Id: IFFByteStream.cpp,v 1.9 1999-03-17 19:24:58 leonb Exp $
 
-// File "$Id: IFFByteStream.cpp,v 1.8 1999-03-16 20:21:29 leonb Exp $"
+// File "$Id: IFFByteStream.cpp,v 1.9 1999-03-17 19:24:58 leonb Exp $"
 // -- Implementation of IFFByteStream
 // - Author: Leon Bottou, 06/1998
 
