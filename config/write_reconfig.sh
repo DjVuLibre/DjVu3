6b853d834f4e99471c4d54a83f798f055653f1e1
leonb <leonb>
1999-10-19 18:48:53 +0000

    Obtain options from variable $CONFIGURATION_OPTIONS

--- config/write_reconfig.sh
+++ config/write_reconfig.sh
@@ -1,13 +1,14 @@
 # This saves the command line to a reconfigure file.
 
-# --- save command line
 if [ -z "$CONFIG_DIR" ] ; then
   echo "You must source functions.sh" 1>&2
   exit 1
 fi
-
+    
+# --- save command line
 echo '#!/bin/sh' > "$TOPBUILDDIR/reconfigure"
 echo "'$TOPSRCDIR/$PROGRAM_NAME' \\" >> "$TOPBUILDDIR/reconfigure"
+set - $CONFIGURATION_OPTIONS
 while [ $# != 0 ] ; do
   echon "'$1' \\" >> "$TOPBUILDDIR/reconfigure"
   shift
