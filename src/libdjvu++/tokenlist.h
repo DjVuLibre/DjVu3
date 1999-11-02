c97db011b0f4f0028408c9ca07780dca415f2b48
bcr <bcr>
1999-11-02 22:00:26 +0000

    Added in the basic class description.

--- src/libdjvu++/tokenlist.h
+++ src/libdjvu++/tokenlist.h
@@ -9,7 +9,7 @@
 //C- AT&T, you have an infringing copy of this software and cannot use it
 //C- without violating AT&T's intellectual property rights.
 //C-
-//C- $Id: tokenlist.h,v 1.1 1999-11-02 21:44:58 bcr Exp $
+//C- $Id: tokenlist.h,v 1.2 1999-11-02 22:00:26 bcr Exp $
  
 
 #ifndef __DJVUTOKENLIST_H__
@@ -35,9 +35,14 @@
 //
 // \end{verbatim} */
 //** @memo tokenlist header file. */
-//** @version $Id: tokenlist.h,v 1.1 1999-11-02 21:44:58 bcr Exp $ */
+//** @version $Id: tokenlist.h,v 1.2 1999-11-02 22:00:26 bcr Exp $ */
 //** @author $Author: bcr $ */
+//@{
 
+//** The DjVuTokenList keeps track of string,integer pairs.  One unique     */
+//** interger is assigned per string.  With the integer range stored from   */
+//** zero to the number of strings present.  This is primarily intended to  */
+//** allow a simple mapping between strings and a fixed size array.         */
 class DjVuTokenList
 {
 private:
@@ -48,16 +53,22 @@ private:
   char **Strings;
 public:
   int links;
+  //** Simple Constructor */
   DjVuTokenList() : ListSize(0),NextToken(0),Entry(0),Strings(0),links(0) {};
+  //** Simple Destructor */
   ~DjVuTokenList();
 
-    // This is just an array lookup.  That is the whole point of tokens.
+  //** Lookup up a string given the token */
   inline const char * const GetString(const int token) const
   { return (token<NextToken)?Strings[token]:0; };
 
+  //** Lookup up a token given a string */
   int GetToken(const char name[]) const;
+
+  //** Assign a token if not already assigned and return it given the string */
   int SetToken(const char name[]);
 };
+//@}
 
 #ifdef _DJVUTOKENLIST_H_IMPLEMENTATION_
 class DjVuTokenList::Entries
