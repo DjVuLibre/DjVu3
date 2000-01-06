d769cfd217f06a71f19163ba8450ff1fb866ecbe
bcr <bcr>
2000-01-06 04:28:09 +0000

    Somehow the DjVuAPI file was overwritten (again)!

--- src/libdjvu++/c-wrappers/DjVuAPI.h
+++ src/libdjvu++/c-wrappers/DjVuAPI.h
@@ -1,4 +1,4 @@
-/* File "$Id: DjVuAPI.h,v 1.17 2000-01-05 19:39:48 praveen Exp $"
+/* File "$Id: DjVuAPI.h,v 1.18 2000-01-06 04:28:09 bcr Exp $"
  *
  * The main header file for the DjVu API
  */
@@ -8,8 +8,8 @@
 
 /* 
  * $Log: DjVuAPI.h,v $
- * Revision 1.17  2000-01-05 19:39:48  praveen
- * *** empty log message ***
+ * Revision 1.18  2000-01-06 04:28:09  bcr
+ * Somehow the DjVuAPI file was overwritten (again)!
  *
  * Revision 1.16  2000/01/05 19:34:24  bcr
  * Generic header file.
@@ -185,15 +185,9 @@ extern "C" {
 #define DLLEXPORT /**/
 #endif /* DJVU_STATIC_LIBRARY */
 
-#ifdef BUILD_LIB
-#ifndef DJVUAPI
-#define DJVUAPI DLLEXPORT
-#endif  /*DJVUAPI*/
-#else
 #ifndef DJVUAPI
 #define DJVUAPI DLLIMPORT
-#endif  /*DJVUAPI*/
-#endif  /*BUILD_LIB*/
+#endif
 
 typedef enum {
   asciiBW='1',
