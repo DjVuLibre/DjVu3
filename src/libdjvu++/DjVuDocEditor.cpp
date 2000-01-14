399604c6e1c821123619844dd0459dc3768b35da
bcr <bcr>
2000-01-14 15:04:19 +0000

    Added missing AT&T forms.

--- src/libdjvu++/DjVuDocEditor.cpp
+++ src/libdjvu++/DjVuDocEditor.cpp
@@ -9,7 +9,7 @@
 //C- AT&T, you have an infringing copy of this software and cannot use it
 //C- without violating AT&T's intellectual property rights.
 //C-
-//C- $Id: DjVuDocEditor.cpp,v 1.20 2000-01-10 21:34:11 eaf Exp $
+//C- $Id: DjVuDocEditor.cpp,v 1.21 2000-01-14 15:04:19 bcr Exp $
 
 #ifdef __GNUC__
 #pragma implementation
@@ -1282,6 +1282,8 @@ DjVuDocEditor::save_as(const char * where, bool bundled)
 	 // Can't be very smart here. Simply overwrite the file.
       GP<DjVmDoc> doc=get_djvm_doc();
       DataPool::load_file(save_doc_name);
+      StdioByteStream str_out(file_name, "wb");
+      str_out.writall("AT&T", 4);
       StdioByteStream str(save_doc_name, "wb");
       doc->write(str);
       str.flush();
