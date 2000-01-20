3b616f019c7180a6001d48bac7b6436aeeea1de6
eaf <eaf>
2000-01-20 23:17:21 +0000

    Replaced call to GThread::is_active() with call to GThread::wait_for_finish().

--- src/libdjvu++/DjVuDocument.h
+++ src/libdjvu++/DjVuDocument.h
@@ -9,7 +9,7 @@
 //C- AT&T, you have an infringing copy of this software and cannot use it
 //C- without violating AT&T's intellectual property rights.
 //C-
-//C- $Id: DjVuDocument.h,v 1.53 2000-01-20 22:55:39 praveen Exp $
+//C- $Id: DjVuDocument.h,v 1.54 2000-01-20 23:17:21 eaf Exp $
  
 #ifndef _DJVUDOCUMENT_H
 #define _DJVUDOCUMENT_H
@@ -33,7 +33,7 @@
 
     @memo DjVu document class.
     @author Andrei Erofeev <eaf@research.att.com>, L\'eon Bottou <leonb@research.att.com>
-    @version #$Id: DjVuDocument.h,v 1.53 2000-01-20 22:55:39 praveen Exp $#
+    @version #$Id: DjVuDocument.h,v 1.54 2000-01-20 23:17:21 eaf Exp $#
 */
 
 //@{
@@ -734,9 +734,8 @@ DjVuDocument::wait_for_complete_init(void)
    while(!(flags & DOC_INIT_FAILED) &&
 	 !(flags & DOC_INIT_COMPLETE)) flags.wait();
    flags.leave();
-#if THREADMODEL!=COTHREADS
-   while(init_thr.is_active());
-#endif
+   init_thr.wait_for_finish();
+   
    return (flags & DOC_INIT_COMPLETE)!=0;
 }
 
