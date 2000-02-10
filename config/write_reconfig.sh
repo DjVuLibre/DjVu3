84857e9dda08f9a7d087704a0a9a69a14fe273d4
leonb <leonb>
2000-02-10 14:13:01 +0000

    Fixed broken generation of reconfigure when there are options.

--- config/write_reconfig.sh
+++ config/write_reconfig.sh
@@ -4,13 +4,13 @@ if [ -z "$CONFIG_DIR" ] ; then
   echo "You must source functions.sh" 1>&2
   exit 1
 fi
-    
+
 # --- save command line
 echo '#!'"$smartshell" > "$TOPBUILDDIR/reconfigure"
 echo "'$TOPSRCDIR/$PROGRAM_NAME' \\" >> "$TOPBUILDDIR/reconfigure"
 set - $CONFIGURATION_OPTIONS
 while [ $# != 0 ] ; do
-  echon "'$1' \\" >> "$TOPBUILDDIR/reconfigure"
+  echon "'$1' " >> "$TOPBUILDDIR/reconfigure"
   shift
 done
 echo " "  >> "$TOPBUILDDIR/reconfigure"
