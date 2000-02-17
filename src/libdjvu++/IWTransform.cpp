016f2ea2503a1669e7ca370494c8b3c84379f4cb
leonb <leonb>
2000-02-17 00:34:42 +0000

    Fixed small bug for very small images

--- src/libdjvu++/IWTransform.cpp
+++ src/libdjvu++/IWTransform.cpp
@@ -9,7 +9,7 @@
 //C- AT&T, you have an infringing copy of this software and cannot use it
 //C- without violating AT&T's intellectual property rights.
 //C-
-//C- $Id: IWTransform.cpp,v 1.14 1999-11-03 00:51:50 leonb Exp $
+//C- $Id: IWTransform.cpp,v 1.15 2000-02-17 00:34:42 leonb Exp $
 
 #ifdef __GNUC__
 #pragma implementation
@@ -487,7 +487,7 @@ filter_fh(short *p, int w, int h, int rowsize, int scale)
       if (q < e)
         {
           // Special case: x=1
-          a1 = a2 = q[-s];
+          a1 = a2 = a3 = q[-s];
           if (q+s<e)
             a2 = q[s];
           if (q+s3<e)
@@ -524,17 +524,17 @@ filter_fh(short *p, int w, int h, int rowsize, int scale)
           q[-s3] = q[-s3] + ((((b1+b2)<<3)+(b1+b2)-b0-b3+16) >> 5);
           q += s+s;
         }
-      if (q-s3 >= p)
-        while (q-s3 < e)
-          {
-            // Special case  w <= x < w+3
-            b0=b1; 
-            b1=b2; 
-            b2=b3;
-            b3=0;
+      while (q-s3 < e)
+        {
+          // Special case  w <= x < w+3
+          b0=b1; 
+          b1=b2; 
+          b2=b3;
+          b3=0;
+          if (q-s3 >= p)
             q[-s3] = q[-s3] + ((((b1+b2)<<3)+(b1+b2)-b0-b3+16) >> 5);
-            q += s+s;
-          }
+          q += s+s;
+        }
       y += scale;
       p += rowsize;
     }
@@ -622,16 +622,16 @@ filter_bh(short *p, int w, int h, int rowsize, int scale)
           q[-s3] = q[-s3] + ((((b1+b2)<<3)+(b1+b2)-b0-b3+8) >> 4);
           q += s+s;
         }
-      if (q-s3 >= p)
-        while (q-s3 < e)
-          {
-            // Special case  w <= x < w+3
-            b0=b1; 
-            b1=b2; 
-            b2=b3;
+      while (q-s3 < e)
+        {
+          // Special case  w <= x < w+3
+          b0=b1; 
+          b1=b2; 
+          b2=b3;
+          if (q-s3 >= p)
             q[-s3] = q[-s3] + ((b1+b2+1)>>1);
-            q += s+s;
-          }
+          q += s+s;
+        }
       y += scale;
       p += rowsize;
     }
