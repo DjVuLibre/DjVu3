2d726bb322bbcd1db608182f422db0cc07ff4e34
bcr <bcr>
2000-10-10 20:16:18 +0000

    Removed duplicate files.

--- src/libdjvu++/c-wrappers/DjVuVersion.h
+++
@@ -1,6 +0,0 @@
-#define DJVU_VERSION   "3.1"
-#define DJVU_CVS_DATE  "$Date: 2000-09-18 17:10:36 $"
-#define DJVU_CVS_NAME  "$Name:  $"
-#define DJVU_CVS_REV   "$Revision: 1.4 $"
-#define DJVU_CVS_STATE "$State: Exp $"
-
