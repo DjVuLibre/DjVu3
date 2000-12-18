618719799682fade682752dda724c7f96d50769b
bcr <bcr>
2000-12-18 17:14:11 +0000

    Adding DjVuText class definition.

---
+++ src/libdjvu++/DjVuText.cpp
@@ -0,0 +1,630 @@
+//C-  -*- C++ -*-
+//C- DjVu® Reference Library (v. 3.0)
+//C- 
+//C- Copyright © 1999-2000 LizardTech, Inc. All Rights Reserved.
+//C- The DjVu Reference Library is protected by U.S. Pat. No.
+//C- 6,058,214 and patents pending.
+//C- 
+//C- This software is subject to, and may be distributed under, the
+//C- GNU General Public License, Version 2. The license should have
+//C- accompanied the software or you may obtain a copy of the license
+//C- from the Free Software Foundation at http://www.fsf.org .
+//C- 
+//C- The computer code originally released by LizardTech under this
+//C- license and unmodified by other parties is deemed the "LizardTech
+//C- Original Code."
+//C- 
+//C- With respect to the LizardTech Original Code ONLY, and subject
+//C- to any third party intellectual property claims, LizardTech
+//C- grants recipient a worldwide, royalty-free, non-exclusive license
+//C- under patent claims now or hereafter owned or controlled by
+//C- LizardTech that are infringed by making, using, or selling
+//C- LizardTech Original Code, but solely to the extent that any such
+//C- patent(s) is/are reasonably necessary to enable you to make, have
+//C- made, practice, sell, or otherwise dispose of LizardTech Original
+//C- Code (or portions thereof) and not to any greater extent that may
+//C- be necessary to utilize further modifications or combinations.
+//C- 
+//C- The LizardTech Original Code is provided "AS IS" WITHOUT WARRANTY
+//C- OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//C- TO ANY WARRANTY OF NON-INFRINGEMENT, OR ANY IMPLIED WARRANTY OF
+//C- MERCHANTIBILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+//C- 
+// 
+// $Id: DjVuText.cpp,v 1.7 2000-12-18 17:14:11 bcr Exp $
+// $Name:  $
+
+#ifdef __GNUC__
+#pragma implementation
+#endif
+
+#include "DjVuText.h"
+#include "IFFByteStream.h"
+#include "BSByteStream.h"
+#include "debug.h"
+#include <ctype.h>
+
+//***************************************************************************
+//******************************** DjVuTXT **********************************
+//***************************************************************************
+
+const char DjVuTXT::end_of_column    = 013;      // VT: Vertical Tab
+const char DjVuTXT::end_of_region    = 035;      // GS: Group Separator
+const char DjVuTXT::end_of_paragraph = 037;      // US: Unit Separator
+const char DjVuTXT::end_of_line      = 012;      // LF: Line Feed
+
+const int DjVuTXT::Zone::version  = 1;
+
+DjVuTXT::Zone::Zone()
+  : ztype(DjVuTXT::PAGE), text_start(0), text_length(0)
+{
+}
+
+DjVuTXT::Zone *
+DjVuTXT::Zone::append_child()
+{
+  Zone empty;
+  empty.ztype = ztype;
+  empty.text_start = 0;
+  empty.text_length = 0;
+  children.append(empty);
+  return & children[children.lastpos()];
+}
+
+void
+DjVuTXT::Zone::cleartext()
+{
+  text_start = 0;
+  text_length = 0;
+  for (GPosition i=children; i; ++i)
+    children[i].cleartext();
+}
+
+void
+DjVuTXT::Zone::normtext(const char *instr, GString &outstr)
+{
+  if (text_length == 0)
+    {
+      // Descend collecting text below
+      text_start = outstr.length();
+      for (GPosition i=children; i; ++i)
+        children[i].normtext(instr, outstr);
+      text_length = outstr.length() - text_start;
+      // Ignore empty zones
+      if (text_length == 0)
+        return;
+    }
+  else
+    {
+      // Collect text at this level
+      int new_start = outstr.length();
+      outstr = outstr + GString(instr+text_start, text_length);
+      text_start = new_start;
+      // Clear textual information on lower level nodes
+      for (GPosition i=children; i; ++i)
+        children[i].cleartext();
+    }
+  // Determine standard separator
+  char sep;
+  switch (ztype)
+    {
+    case COLUMN:
+      sep = end_of_column; break;
+    case REGION:
+      sep = end_of_region; break;
+    case PARAGRAPH: 
+      sep = end_of_paragraph; break;
+    case LINE:
+      sep = end_of_line; break;
+    case WORD:
+      sep = ' '; break;
+    default:
+      return;
+    }
+  // Add separator if not present yet.
+  if (outstr[text_start+text_length-1] != sep)
+    {
+      outstr = outstr + GString(&sep, 1);
+      text_length += 1;
+    }
+}
+
+unsigned int 
+DjVuTXT::Zone::memuse() const
+{
+  int memuse = sizeof(*this);
+  for (GPosition i=children; i; ++i)
+    memuse += children[i].memuse();
+  return memuse;
+}
+
+
+#ifndef NEED_DECODER_ONLY
+void 
+DjVuTXT::Zone::encode(ByteStream &bs, const Zone * parent, const Zone * prev) const
+{
+  // Encode type
+  bs.write8(ztype);
+  
+  // Modify text_start and bounding rectangle based on the context
+  // (whether there is a previous non-zero same-level-child or parent)
+  int start=text_start;
+  int x=rect.xmin, y=rect.ymin;
+  int width=rect.width(), height=rect.height();
+  if (prev)
+  {
+    if (ztype==PAGE || ztype==PARAGRAPH || ztype==LINE)
+    {
+      // Encode offset from the lower left corner of the previous
+      // child in the coord system in that corner with x to the
+      // right and y down
+      x=x-prev->rect.xmin;
+      y=prev->rect.ymin-(y+height);
+    } else // Either COLUMN or WORD or CHARACTER
+    {
+      // Encode offset from the lower right corner of the previous
+      // child in the coord system in that corner with x to the
+      // right and y up
+      x=x-prev->rect.xmax;
+      y=y-prev->rect.ymin;
+    }
+    start-=prev->text_start+prev->text_length;
+  } else if (parent)
+  {
+    // Encode offset from the upper left corner of the parent
+    // in the coord system in that corner with x to the right and y down
+    x=x-parent->rect.xmin;
+    y=parent->rect.ymax-(y+height);
+    start-=parent->text_start;
+  }
+  // Encode rectangle
+  bs.write16(0x8000+x);
+  bs.write16(0x8000+y);
+  bs.write16(0x8000+width);
+  bs.write16(0x8000+height);
+  // Encode text info
+  bs.write16(0x8000+start);
+  bs.write24(text_length);
+  // Encode number of children
+  bs.write24(children.size());
+  
+  const Zone * prev_child=0;
+  // Encode all children
+  for (GPosition i=children; i; ++i)
+  {
+    children[i].encode(bs, this, prev_child);
+    prev_child=&children[i];
+  }
+}
+#endif
+
+void 
+DjVuTXT::Zone::decode(ByteStream &bs, int maxtext,
+		      const Zone * parent, const Zone * prev)
+{
+  // Decode type
+  ztype = (ZoneType) bs.read8();
+  if ( ztype<PAGE || ztype>CHARACTER )
+    G_THROW("DjVuText.corrupt_text");
+
+  // Decode coordinates
+  int x=(int) bs.read16()-0x8000;
+  int y=(int) bs.read16()-0x8000;
+  int width=(int) bs.read16()-0x8000;
+  int height=(int) bs.read16()-0x8000;
+
+  // Decode text info
+  text_start = (int) bs.read16()-0x8000;
+  text_length = bs.read24();
+  if (prev)
+  {
+    if (ztype==PAGE || ztype==PARAGRAPH || ztype==LINE)
+    {
+      x=x+prev->rect.xmin;
+      y=prev->rect.ymin-(y+height);
+    } else // Either COLUMN or WORD or CHARACTER
+    {
+      x=x+prev->rect.xmax;
+      y=y+prev->rect.ymin;
+    }
+    text_start+=prev->text_start+prev->text_length;
+  } else if (parent)
+  {
+    x=x+parent->rect.xmin;
+    y=parent->rect.ymax-(y+height);
+    text_start+=parent->text_start;
+  }
+  rect=GRect(x, y, width, height);
+  
+  // Get children size
+  int size = bs.read24();
+
+  // Checks
+  if (rect.isempty() || text_start<0 || text_start+text_length>maxtext )
+    G_THROW("DjVuText.corrupt_text");
+
+  // Process children
+  const Zone * prev_child=0;
+  children.empty();
+  while (size-- > 0) 
+  {
+    Zone *z = append_child();
+    z->decode(bs, maxtext, this, prev_child);
+    prev_child=z;
+  }
+}
+
+void 
+DjVuTXT::normalize_text()
+{
+  GString newtextUTF8;
+  page_zone.normtext( (const char*)textUTF8, newtextUTF8 );
+  textUTF8 = newtextUTF8;
+}
+
+int 
+DjVuTXT::has_valid_zones() const
+{
+  if (!textUTF8)
+    return false;
+  if (page_zone.children.isempty() || page_zone.rect.isempty()) 
+    return false;
+  return true;
+}
+
+
+#ifndef NEED_DECODER_ONLY
+void 
+DjVuTXT::encode(ByteStream &bs) const
+{
+  if (! textUTF8 )
+    G_THROW("DjVuText.no_text");
+  // Encode text
+  int textsize = textUTF8.length();
+  bs.write24( textsize );
+  bs.writall( (void*)(const char*)textUTF8, textsize );
+  // Encode zones
+  if (has_valid_zones())
+  {
+    bs.write8(Zone::version);
+    page_zone.encode(bs);
+  }
+}
+#endif
+
+void 
+DjVuTXT::decode(ByteStream &bs)
+{
+  // Read text
+  textUTF8.empty();
+  int textsize = bs.read24();
+  char *buffer = textUTF8.getbuf(textsize);
+  int readsize = bs.read(buffer,textsize);
+  buffer[readsize] = 0;
+  if (readsize < textsize)
+    G_THROW("DjVuText.corrupt_chunk");
+  // Try reading zones
+  unsigned char version;
+  if ( bs.read( (void*) &version, 1 ) == 1) 
+  {
+    if (version != Zone::version)
+      G_THROW("DjVuText.bad_version\t"+GString(version));
+    page_zone.decode(bs, textsize);
+  }
+}
+
+GP<DjVuTXT> 
+DjVuTXT::copy(void) const
+{
+  return new DjVuTXT(*this);
+}
+
+
+bool
+DjVuTXT::search_zone(const Zone * zone, int start, int & end) const
+      // Will return TRUE if 'zone' contains beginning of the text
+      // at 'start'. In this case it will also modify the 'end'
+      // to point to the first character beyond the zone
+{
+  if (start>=zone->text_start && start<zone->text_start+zone->text_length)
+  {
+    if (end>zone->text_start+zone->text_length)
+      end=zone->text_start+zone->text_length;
+    return true;
+  }
+  return false;
+}
+
+DjVuTXT::Zone *
+DjVuTXT::get_smallest_zone(int max_type, int start, int & end) const
+      // Will return the smallest zone with type up to max_type containing
+      // the text starting at start. If anything is found, end will
+      // be modified to point to the first character beyond the zone
+{
+  if (!search_zone(&page_zone, start, end)) return 0;
+  
+  const Zone * zone=&page_zone;
+  while(zone->ztype<max_type)
+  {
+    GPosition pos;
+    for(pos=zone->children;pos;++pos)
+      if (search_zone(&zone->children[pos], start, end))
+        break;
+      if (pos) 
+        zone=&zone->children[pos];
+      else 
+        break;
+  }
+  
+  return (Zone *) zone;
+}
+
+GList<DjVuTXT::Zone *>
+DjVuTXT::find_zones(int string_start, int string_length) const
+      // For the string starting at string_start of length string_length
+      // the function will generate a list of smallest zones of the
+      // same type and will return it
+{
+  GList<Zone *> zone_list;
+  
+  {
+    // Get rid of the leading and terminating spaces
+    int start=string_start;
+    int end=string_start+string_length;
+    while(start<end && isspace(textUTF8[start]))
+      start++;
+    while(end>start && isspace(textUTF8[end-1]))
+      end--;
+    if (start==end)
+      return zone_list;
+    string_start=start;
+    string_length=end-start;
+  }
+  
+  int zone_type=CHARACTER;
+  while(zone_type>=PAGE)
+  {
+    int start=string_start;
+    int end=string_start+string_length;
+    
+    while(true)
+    {
+      while(start<end && isspace(textUTF8[start])) start++;
+      if (start==end) break;
+      
+      Zone * zone=get_smallest_zone(zone_type, start, end);
+      if (zone && zone_type==zone->ztype)
+      {
+        zone_list.append(zone);
+        start=end;
+        end=string_start+string_length;
+      } else
+      {
+        zone_type--;
+        zone_list.empty();
+        break;
+      }
+    }
+    if (zone_list.size()) break;
+  }
+  return zone_list;
+}
+
+static inline bool
+is_blank(char ch)
+{
+   return
+       isspace(ch) ||
+       ch==DjVuTXT::end_of_column ||
+       ch==DjVuTXT::end_of_region ||
+       ch==DjVuTXT::end_of_paragraph ||
+       ch==DjVuTXT::end_of_line;
+}
+
+static inline bool
+is_separator(char ch, bool whole_word)
+   // Returns TRUE, if the 'ch' is a separator.
+   // If 'whole_word' is TRUE, the separators also include
+   // all punctuation characters. Otherwise, they include only
+   // white characters.
+{
+   return is_blank(ch) ||
+	  whole_word && strchr("-!,.;:?\"'", ch);
+}
+
+static inline bool
+chars_equal(char ch1, char ch2, bool match_case)
+{
+   if (is_blank(ch1) && is_blank(ch2))
+      return true;
+
+   return match_case ? (ch1==ch2) : (toupper(ch1)==toupper(ch2));
+}
+
+static bool
+equal(const char * txt, const char * str, bool match_case, int * length)
+      // Returns TRUE, if 'txt' contains the same words as the 'str'
+      // in the same order (but possibly with different number of
+      // separators between words). The 'separators' in this
+      // function are blank and 'end_of_...' characters.
+      //
+      // If search has been successful, the function will make 'length'
+      // contain the index of the first character in the 'txt' beyond the
+      // found string
+      //
+      // NOTE, that it may be different from strlen(str) because of different
+      // number of spaces between words in 'str' and 'txt'
+{
+   const char * str_ptr=str;
+   const char * txt_ptr=txt;
+   while(*str_ptr)
+   {
+      while(*str_ptr && is_blank(*str_ptr)) str_ptr++;
+      while(*str_ptr && *txt_ptr && !is_blank(*str_ptr))
+	 if (chars_equal(*txt_ptr, *str_ptr, match_case))
+	 {
+	    txt_ptr++; str_ptr++;
+	 } else break;
+      if (!*str_ptr)
+	 break;
+      if (!*txt_ptr || !is_blank(*str_ptr) || !is_blank(*txt_ptr))
+	 return false;
+      while(*txt_ptr && is_blank(*txt_ptr)) txt_ptr++;
+   }
+   if (length) *length=txt_ptr-txt;
+   return true;
+}
+
+GList<DjVuTXT::Zone *>
+DjVuTXT::search_string(const char * string, int & from, bool search_fwd,
+		       bool match_case, bool whole_word) const
+{
+  GString local_string;
+  
+  const char * ptr;
+  
+  // Get rid of the leading separators
+  for(ptr=string;*ptr;ptr++)
+    if (!is_separator(*ptr, whole_word))
+      break;
+  local_string=ptr;
+  
+  // Get rid of the terminating separators
+  while(local_string.length() &&
+        is_separator(local_string[(int)(local_string.length())-1], whole_word))
+    local_string.setat(local_string.length()-1, 0);
+  
+  string=local_string;
+  
+  if (whole_word)
+  {
+    // Make sure that the string does not contain any
+    // separators in the middle
+    for(const char * ptr=string;*ptr;ptr++)
+      if (is_separator(*ptr, true))
+        G_THROW("DjVuText.one_word");
+  }
+  
+  int string_length=strlen(string);
+  bool found=false;
+  
+  if (string_length==0 || textUTF8.length()==0 ||
+    string_length>(int) textUTF8.length())
+  {
+    if (search_fwd) from=textUTF8.length();
+    else from=-1;
+    return GList<Zone *>();
+  }
+  
+  int real_str_len;
+  if (search_fwd)
+  {
+    if (from<0) from=0;
+    while(from<(int) textUTF8.length())
+    {
+      if (equal((const char *) textUTF8+from, string, match_case, &real_str_len))
+      {
+        if (!whole_word ||
+          (from==0 || is_separator(textUTF8[from-1], true)) &&
+          (from+real_str_len==(int) textUTF8.length() ||
+          is_separator(textUTF8[from+real_str_len], true)))
+        {
+          found=true;
+          break;
+        }
+      }
+      from++;
+    }      
+  }
+  else
+  {
+    if (from>(int) textUTF8.length()-1) from=(int) textUTF8.length()-1;
+    while(from>=0)
+    {
+      if (equal((const char *) textUTF8+from, string, match_case, &real_str_len))
+      {
+        if (!whole_word ||
+          (from==0 || is_separator(textUTF8[from-1], true)) &&
+          (from+real_str_len==(int) textUTF8.length() ||
+          is_separator(textUTF8[from+real_str_len], true)))
+        {
+          found=true;
+          break;
+        }
+      }
+      from--;
+    }
+  }
+  
+  if (found) return find_zones(from, real_str_len);
+  else return GList<Zone *>();
+}
+
+unsigned int 
+DjVuTXT::get_memory_usage() const
+{
+  return sizeof(*this) + textUTF8.length() + page_zone.memuse() - sizeof(page_zone); 
+}
+
+
+
+//***************************************************************************
+//******************************** DjVuText *********************************
+//***************************************************************************
+
+void
+DjVuText::decode(ByteStream &bs)
+{
+  GString chkid;
+  IFFByteStream iff(bs);
+  while( iff.get_chunk(chkid) )
+  {
+    if (chkid == "TXTa")
+    {
+      if (txt)
+        G_THROW("DjVuText.dupl_text");
+      txt = new DjVuTXT;
+      txt->decode(iff);
+    }
+    else if (chkid == "TXTz")
+    {
+      if (txt)
+        G_THROW("DjVuText.dupl_text");
+      txt = new DjVuTXT;
+      BSByteStream bsiff(iff);
+      txt->decode(bsiff);
+    }
+    // Add decoding of other chunks here
+    iff.close_chunk();
+  }
+}
+
+void
+DjVuText::encode(ByteStream &bs)
+{
+  IFFByteStream iff(bs);
+  if (txt)
+    {
+      iff.put_chunk("TXTz");
+      {
+	BSByteStream bsiff(iff,50);
+	txt->encode(bsiff);
+      }
+      iff.close_chunk();
+    }
+  // Add encoding of other chunks here
+}
+
+
+GP<DjVuText>
+DjVuText::copy(void) const
+{
+   GP<DjVuText> text= new DjVuText;
+      // Copy any primitives (if any)
+   *text=*this;
+      // Copy each substructure
+   if (txt)
+     text->txt = txt->copy();
+   return text;
+}
+
---
+++ src/libdjvu++/DjVuText.h
@@ -0,0 +1,227 @@
+//C-  -*- C++ -*-
+//C- DjVu® Reference Library (v. 3.0)
+//C- 
+//C- Copyright © 1999-2000 LizardTech, Inc. All Rights Reserved.
+//C- The DjVu Reference Library is protected by U.S. Pat. No.
+//C- 6,058,214 and patents pending.
+//C- 
+//C- This software is subject to, and may be distributed under, the
+//C- GNU General Public License, Version 2. The license should have
+//C- accompanied the software or you may obtain a copy of the license
+//C- from the Free Software Foundation at http://www.fsf.org .
+//C- 
+//C- The computer code originally released by LizardTech under this
+//C- license and unmodified by other parties is deemed the "LizardTech
+//C- Original Code."
+//C- 
+//C- With respect to the LizardTech Original Code ONLY, and subject
+//C- to any third party intellectual property claims, LizardTech
+//C- grants recipient a worldwide, royalty-free, non-exclusive license
+//C- under patent claims now or hereafter owned or controlled by
+//C- LizardTech that are infringed by making, using, or selling
+//C- LizardTech Original Code, but solely to the extent that any such
+//C- patent(s) is/are reasonably necessary to enable you to make, have
+//C- made, practice, sell, or otherwise dispose of LizardTech Original
+//C- Code (or portions thereof) and not to any greater extent that may
+//C- be necessary to utilize further modifications or combinations.
+//C- 
+//C- The LizardTech Original Code is provided "AS IS" WITHOUT WARRANTY
+//C- OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//C- TO ANY WARRANTY OF NON-INFRINGEMENT, OR ANY IMPLIED WARRANTY OF
+//C- MERCHANTIBILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+//C- 
+// 
+// $Id: DjVuText.h,v 1.5 2000-12-18 17:14:11 bcr Exp $
+// $Name:  $
+
+#ifndef _DJVUTEXT_H
+#define _DJVUTEXT_H
+
+
+
+/** @name DjVuText.h
+
+    Files #"DjVuText.h"# and #"DjVuText.cpp"# implement the mechanism for
+    text in DjVuImages.
+
+    This file implements annotations understood by the DjVu plugins 
+    and encoders.
+
+
+    using: contents of #TXT*# chunks.
+
+    Contents of the #FORM:TEXT# should be passed to \Ref{DjVuText::decode}()
+    for parsing, which initializes \Ref{DjVuText::TXT} 
+    and fills them with decoded data. 
+    @memo Implements support for DjVuImage hidden text.
+    @author Andrei Erofeev <eaf@geocities.com>
+    @version
+    #$Id: DjVuText.h,v 1.5 2000-12-18 17:14:11 bcr Exp $# */
+//@{
+
+#ifdef __GNUC__
+#pragma interface
+#endif
+
+#include "GMapAreas.h"
+
+class ByteStream;
+
+// -------- DJVUTXT --------
+
+/** Description of the text contained in a DjVu page.  This class contains the
+    textual data for the page.  It describes the text as a hierarchy of zones
+    corresponding to page, column, region, paragraph, lines, words, etc...
+    The piece of text associated with each zone is represented by an offset
+    and a length describing a segment of a global UTF8 encoded string.  */
+
+class DjVuTXT : public GPEnabled
+{
+public:
+  /** These constants are used to tell what a zone describes.
+      This can be useful for a copy/paste application. 
+      The deeper we go into the hierarchy, the higher the constant. */
+  enum ZoneType { PAGE=1, COLUMN=2, REGION=3, PARAGRAPH=4, LINE=5, WORD=6, CHARACTER=7 };
+  /** Data structure representing document textual components.
+      The text structure is represented by a hierarchy of rectangular zones. */
+  struct Zone 
+  {
+    Zone();
+    /** Type of the zone. */
+    enum ZoneType ztype;
+    /** Rectangle spanned by the zone */
+    GRect rect;
+    /** Position of the zone text in string #textUTF8#. */
+    int text_start;
+    /** Length of the zone text in string #textUTF8#. */
+    int text_length;
+    /** List of children zone. */
+    GList<Zone> children;
+    /** Appends another subzone inside this zone.  The new zone is initialized
+        with an empty rectangle, empty text, and has the same type as this
+        zone. */
+    Zone *append_child();
+  private:
+    friend class DjVuTXT;
+    void cleartext();
+    void normtext(const char *instr, GString &outstr);
+    unsigned int memuse() const;
+    static const int version;
+    void encode(ByteStream &bs, const Zone * parent=0, const Zone * prev=0) const;
+    void decode(ByteStream &bs, int maxtext,
+	            	const Zone * parent=0, const Zone * prev=0);
+  };
+  /** Textual data for this page.  
+      The content of this string is encoded using the UTF8 code.
+      This code corresponds to ASCII for the first 127 characters.
+      Columns, regions, paragraph and lines are delimited by the following
+      control character:
+      \begin{tabular}{lll}
+        {\bf Name} & {\bf Octal} & {\bf Ascii name} \\\hline\\
+        {\tt DjVuText::end_of_column}    & 013 & VT, Vertical Tab \\
+        {\tt DjVuText::end_of_region}    & 035 & GS, Group Separator \\
+        {\tt DjVuText::end_of_paragraph} & 037 & US, Unit Separator \\
+        {\tt DjVuText::end_of_line}      & 012 & LF: Line Feed
+      \end{tabular} */
+  GString textUTF8;
+  static const char end_of_column    ;      // VT: Vertical Tab
+  static const char end_of_region    ;      // GS: Group Separator
+  static const char end_of_paragraph ;      // US: Unit Separator
+  static const char end_of_line      ;      // LF: Line Feed
+  /** Main zone in the document.
+      This zone represent the page. */
+  Zone page_zone;
+  /** Tests whether there is a meaningful zone hierarchy. */
+  int has_valid_zones() const;
+  /** Normalize textual data.  Assuming that a zone hierarchy has been built
+      and represents the reading order.  This function reorganizes the string
+      #textUTF8# by gathering the highest level text available in the zone
+      hierarchy.  The text offsets and lengths are recomputed for all the
+      zones in the hierarchy. Separators are inserted where appropriate. */
+  void normalize_text();
+  /** Encode data for a TXT chunk. */
+  void encode(ByteStream &bs) const;
+  /** Decode data from a TXT chunk. */
+  void decode(ByteStream &bs);
+  /** Returns a copy of this object. */
+  GP<DjVuTXT> copy(void) const;
+  /** Searches the TXT chunk for the given string and returns a list of
+      the smallest zones covering the text.
+      @param string String to be found. May contain spaces as word separators.
+      @param start_pos Position where to start searching. It may be negative
+             or it may be bigger than the length of the \Ref{textUTF8}
+	     string. If the #start_pos# is out of bounds, it will be fixed
+	     before starting the search
+	     \begin{itemize}
+	        \item If #start_pos# is negative and we search forward,
+		      the #start_pos# will be reset to #0#.
+		\item If #start_pos# is too big and we search backward,
+		      the #start_pos# will be reset to the #textUTF8.length()-1#.
+		\item Otherwise the #start_pos# will remain unchanged, and
+		      nothing will be found.
+	     \end{itemize}
+	     
+	     If the function manages to find an occurrence of the string,
+	     it will modify the #start_pos# to point to it. If no match has
+	     been found, the #start_pos# will be reset to some big number
+	     if searching forward and #-1# otherwise.
+      @param search_fwd #TRUE# means to search forward. #FALSE# - backward.
+      @param match_case If set to #FALSE# the search will be case-insensitive.
+      @param whole_word If set to #TRUE# the function will try to find
+	     a whole word matching the passed string. The word separators
+	     are all blank and punctuation characters. The passed
+	     string may {\bf not} contain word separators, that is it
+	     {\bf must} be a whole word.
+
+      {\bf WARNING:} The returned list contains pointers to Zones.
+      {\bf DO NOT DELETE} these Zones.
+      */
+  GList<Zone *> search_string(const char * string, int & start_pos,
+			      bool search_fwd, bool match_case,
+			      bool whole_word=false) const;
+  /** Returns the number of bytes needed by this data structure. It's
+      used by caching routines to estimate the size of a \Ref{DjVuImage}. */
+  unsigned int get_memory_usage() const;
+private:
+  bool		search_zone(const Zone * zone, int start, int & end) const;
+  Zone	*	get_smallest_zone(int max_type, int start, int & end) const;
+  GList<Zone *>	find_zones(int string_start, int string_length) const;
+};
+
+
+class DjVuText : public GPEnabled
+{
+public:
+   GP<DjVuTXT>  txt;
+
+      /** Decodes a sequence of annotation chunks and merges contents of every
+	  chunk with previously decoded information. This function
+	  should be called right after applying \Ref{IFFByteStream::get_chunk}()
+	  to data from #FORM:TEXT#. */
+   void decode(ByteStream & bs);
+
+      /** Encodes all annotations back into a sequence of chunks to be put
+	  inside a #FORM:TEXT#. */
+   void	encode(ByteStream & bs);
+
+      /// Returns a copy of this object
+   GP<DjVuText>	copy(void) const;
+
+      /** Returns the number of bytes needed by this data structure. It's
+	  used by caching routines to estimate the size of a \Ref{DjVuImage}. */
+   inline unsigned int get_memory_usage() const;
+};
+
+//@}
+
+inline unsigned int
+DjVuText::get_memory_usage() const
+{
+  return (txt)?(txt->get_memory_usage()):0;
+}
+
+
+// ----- THE END
+#endif
+
+
