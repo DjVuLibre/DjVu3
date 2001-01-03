2acd053c78349aac4dd81d1a02bcca90c29c3e54
bcr <bcr>
2001-01-03 23:03:57 +0000

    Fixed earlier problems.

--- src/libdjvu++/GSmartPointer.h
+++ src/libdjvu++/GSmartPointer.h
@@ -31,7 +31,7 @@
 //C- MERCHANTIBILITY OR FITNESS FOR A PARTICULAR PURPOSE.
 //C- 
 // 
-// $Id: GSmartPointer.h,v 1.31 2001-01-03 22:54:13 bcr Exp $
+// $Id: GSmartPointer.h,v 1.32 2001-01-03 23:03:57 bcr Exp $
 // $Name:  $
 
 #ifndef _GSMARTPOINTER_H_
@@ -54,7 +54,7 @@
     L\'eon Bottou <leonb@research.att.com> -- initial implementation\\
     Andrei Erofeev <eaf@geocities.com> -- bug fix.
     @version 
-    #$Id: GSmartPointer.h,v 1.31 2001-01-03 22:54:13 bcr Exp $# 
+    #$Id: GSmartPointer.h,v 1.32 2001-01-03 23:03:57 bcr Exp $# 
     @args
 */
 //@{
@@ -93,7 +93,6 @@ GPBufferBase::GPBufferBase(void *&xptr,const size_t n,const size_t t) : ptr(xptr
 inline
 GPBufferBase::~GPBufferBase()
 {
-  fprintf(stderr,"delete %x\n",ptr);
   ::operator delete(ptr);
 }
 
